a530c2157491e3932335cd1a0d779f6a3ecbffd4
Kévin Dunglas <dunglas@gmail.com>
2017-09-13 22:24:07 +0200

    Revamp the standard edition: Flex and admin containers

---
+++ api/config/bundles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle' => ['all' => true],
+    'Nelmio\CorsBundle\NelmioCorsBundle' => ['all' => true],
+];
