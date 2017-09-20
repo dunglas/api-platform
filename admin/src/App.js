73c0fb4ab1960a7dc739f7234173fdee8e83a6d8
Kévin Dunglas <dunglas@gmail.com>
2017-09-20 14:49:20 +0200

    Fix some comments and bugs. Add E2E tests.

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,6 @@ import { HydraAdmin } from '@api-platform/admin';
 
 class App extends Component {
   render() {
-    console.log(process.env.REACT_APP_API_ENTRYPOINT);
     return <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/> // Replace with your own API entrypoint
   }
 }
