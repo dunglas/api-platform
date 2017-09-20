803c39255a6c9bb15239c277e81cb05766f57b75
Kévin Dunglas <dunglas@gmail.com>
2017-09-20 02:11:18 +0200

    Add SSL proxy

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,6 +3,7 @@ import { HydraAdmin } from '@api-platform/admin';
 
 class App extends Component {
   render() {
+    console.log(process.env.REACT_APP_API_ENTRYPOINT);
     return <HydraAdmin entrypoint={process.env.REACT_APP_API_ENTRYPOINT}/> // Replace with your own API entrypoint
   }
 }
