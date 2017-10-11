5153e45e835d2aee59589658b76943fdd23cb142
Kévin Dunglas <dunglas@gmail.com>
2017-10-11 22:49:50 +0200

    Improvements

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,11 @@ import thunk from 'redux-thunk';
 import { reducer as form } from 'redux-form';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
-import { syncHistoryWithStore, routerReducer as routing } from 'react-router-redux'
+import { syncHistoryWithStore, routerReducer as routing } from 'react-router-redux';
+// Bootstrap and Font Awesome (optional)
+// Run `yarn add bootstrap@4.0.0-beta font-awesome` and uncomment the next two lines
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'font-awesome/css/font-awesome.css';
 import registerServiceWorker from './registerServiceWorker';
 // Import your reducers and routes here
 import Welcome from './Welcome';
